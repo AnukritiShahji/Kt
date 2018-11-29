55f5d960d2dd541eb7b8be195ff35fe445545e14
anukritis <anukritis@VZD4061020646.infics.com>
2018-11-29 18:42:12 +0530

    for loop

---
+++ KTDemo1/src/org/anukriti/PatternDemo.java
@@ -0,0 +1,18 @@
+package org.anukriti;
+
+public class PatternDemo {
+
+	public static void main(String[] args) 
+	{
+	for(int i=0;i<=2;i++)
+	{
+		for(int j=0;j<=4;j++)
+		{
+			System.out.println("i="+i+" "+"j="+j);
+		}
+		System.out.println("");
+	}
+
+	}
+
+}
